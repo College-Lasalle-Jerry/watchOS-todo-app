6b07c80bb19e6da184276d19da86bccd098b2cb6
centinna-l <jerryjoy99@gmail.com>
2025-02-17 21:28:53 -0500

    Initial Commit

---
+++ WTodo App Watch App/WTodo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTodo_AppApp.swift
+//  WTodo App Watch App
+//
+//  Created by Jerry Joy on 2025-02-17.
+//
+
+import SwiftUI
+
+@main
+struct WTodo_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
